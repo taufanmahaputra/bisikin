e8fa54e90fa67a392ddc96e4724d443d6e37d08b
Taufan Mahaputra <hacker.indo62@gmail.com>
2019-04-11 13:47:56 +0700

    Add route for send multicast message

--- webhooks/line/routes/message.js
+++ webhooks/line/routes/message.js
@@ -8,9 +8,19 @@ router.post('/', (req, res) => {
   const text = req.body.text;
 
   line.client.pushMessage(userId, { type: 'text', text: text })
-    .then(() => res.json({messsage: `Successfully sent the message to ${userId}`, status: 'OK', code: 5}))
+    .then(() => res.json({message: `Successfully sent the message to ${userId}`, status: 'OK', code: 5}))
     .catch(() => res.json({message: 'Cannot send the message', status: 'ERROR', code: 1}))
 
 });
+
+/* POST multicast push message */
+router.post('/multicast', (req, res) => {
+  const userIds = req.body.to;
+  const text = req.body.text;
+
+  line.client.multicast(userIds, { type: 'text', text: text })
+    .then(() => res.json({message: `Successfully sent the message`, status: 'OK', code: 5}))
+    .catch(() => res.json({message: 'Cannot send the message', status: 'ERROR', code: 1}))
+})
 
 module.exports = router;
